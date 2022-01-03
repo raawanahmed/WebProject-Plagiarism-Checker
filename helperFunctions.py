a8648c2eb3a38eacc0c5e72131b28c2eaaa432d6
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2022-01-03 23:44:26 +0200

    add comments

--- helperFunctions.py
+++ helperFunctions.py
@@ -4,28 +4,23 @@ import os
 import numpy as np
 
 
-
 def calcFrequency(word, wordsList):
     freq = 0
-    for w in wordsList:
-        if word == w:
+    for curWord in wordsList:
+        if word == curWord:
             freq += 1
     return freq
 
 
 def calcVectorMagnitude(TFarray):
     vectorMagnitude = 0
-    for i in range(len(TFarray)):
-        vectorMagnitude += TFarray[i] ** 2
+    for idx in range(len(TFarray)):
+        vectorMagnitude += TFarray[idx] ** 2
     return vectorMagnitude
 
 
-def calcDotProduct(inputQueryTF, databaseTF):
-    # dotProduct = 0
-    # for i in range(len(inputQueryTF)):
-    #     dotProduct += inputQueryTF[i] * databaseTF[i]
-    return np.dot(inputQueryTF, databaseTF)
-
+def calcDotProduct(listOfText1, listOfText2):
+    return np.dot(listOfText1, listOfText2)
 
 
 def extractFiles(filesPath: str):
@@ -38,8 +33,9 @@ def extractFiles(filesPath: str):
         nameOfEachFile.add(txtFiles[int(i)])
     return nameOfEachFile, pathOfFiles
 
+
 def extractQueryText(htmlElementName: str):
-    #extract text entered by user given the htmlElementName
+    # extract text entered by user given the htmlElementName
 
     allWords = set()
     inputQuery = request.form[htmlElementName]
@@ -56,32 +52,46 @@ def extractQueryText(htmlElementName: str):
 
 
 def clearAllExistingFiles():
-    deleteUserInputFiles()  # delete files that have been uploaded when comparing multiple files
-    deleteTxtFiles()        # delete files that have been uploaded when comparing input text with a file
+    # delete files that have been uploaded when comparing multiple files
+    deleteUserInputFiles()
+    # delete files that have been uploaded when comparing input text with a file
+    deleteTxtFiles()
 
-def renderPage(fileName = "", query="", query1="", query2="", output="", warningMessage="", greetingMessage="", results=""):
-    #clear all input files is there were any, then render the html page
+
+def renderPage(fileName="", query="", query1="", query2="", output="", warningMessage="", greetingMessage="", results=""):
+    # clear all input files is there were any, then render the html page
     clearAllExistingFiles()
-    return render_template(fileName, query=query, query1=query1, query2=query2, output=output, 
-            warningMessage=warningMessage, greetingMessage=greetingMessage, results=results)
+    return render_template(fileName, query=query, query1=query1, query2=query2, output=output,
+                           warningMessage=warningMessage, greetingMessage=greetingMessage, results=results)
+
 
 def deleteTxtFiles():
     files = [doc for doc in os.listdir() if doc.endswith('.txt')]
+    # check if there is files in directory
     if len(files) > 0:
         for file in files:
             os.remove(file)
 
 
 def deleteUserInputFiles():
-    #delete all files that the user may have input while checking
-    #for plagiarism if they exist
+    # delete all files that the user may have input while checking
+    # for plagiarism if they exist
     directory = "./files"
     if not os.path.exists(directory):
         return
-        
+
     files_in_directory = os.listdir(directory)
-    filtered_files = [file for file in files_in_directory if file.endswith(".txt")]
+    filtered_files = [
+        file for file in files_in_directory if file.endswith(".txt")]
     if len(filtered_files) > 0:
         for file in filtered_files:
             path_to_file = os.path.join(directory, file)
             os.remove(path_to_file)
+
+
+def cosineSimilarity(listOfWords1, listOfWords2):
+    dot_product = calcDotProduct(listOfWords1, listOfWords2)
+    magnitude_x = np.sqrt(np.sum(listOfWords1 ** 2))
+    magnitude_y = np.sqrt(np.sum(listOfWords2 ** 2))
+    cos_similarity = dot_product / (magnitude_x * magnitude_y)
+    return cos_similarity
