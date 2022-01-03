3464a46af5cc3af5022c84204a6896bba2394297
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2022-01-04 00:00:39 +0200

    fix bug

--- plagBetweenTwoInputQueries.py
+++ plagBetweenTwoInputQueries.py
@@ -5,24 +5,23 @@ from Constants import ERROR_NO_INPUT
 
 from helperFunctions import calcDotProduct, calcFrequency, calcVectorMagnitude, extractQueryText
 
+
 def calcSimilarityBetweenTwoQueries():
     firstInputWords, firstInputWordsList = extractQueryText('query1')
     secondInputWords, secondInputWordsList = extractQueryText('query2')
 
-    #check if data is present in both queries 
+    # check if data is present in both queries
     if firstInputWords == -1 or secondInputWords == -1:
         return ERROR_NO_INPUT
-
 
     allWords = set()
     allWords = firstInputWords.union(secondInputWords)
 
     # TF frequency of word i in document j
-    firstInputTF = []
-    secondInputTF = []
+    firstInputTfIdf = []
+    secondInputTfIdf = []
     for word in allWords:
         cnt = 0
-      
         if word in firstInputWordsList:
             cnt += 1
         if word in secondInputWordsList:
@@ -31,14 +30,16 @@ def calcSimilarityBetweenTwoQueries():
 
         if idf == 0:
             idf = 1
-        firstInputInputTfCounter = calcFrequency(word, firstInputWordsList) * idf    
-        secondInputTFCounter = calcFrequency(word, secondInputWordsList)*idf
-        firstInputTF.append(firstInputInputTfCounter)
-        secondInputTF.append(secondInputTFCounter)
+        firstInputInputTfCounter = calcFrequency(
+            word, firstInputWordsList) * idf
+        secondInputTfCounter = calcFrequency(word, secondInputWordsList)*idf
+        firstInputTfIdf.append(firstInputInputTfCounter)
+        secondInputTfIdf.append(secondInputTfCounter)
 
-    dotProduct = calcDotProduct(firstInputTF, secondInputTF)
-    queryVectorMagnitude = math.sqrt(calcVectorMagnitude(firstInputTF))
-    secondInputVectorMagnitude = math.sqrt(calcVectorMagnitude(secondInputTF))
+    dotProduct = calcDotProduct(firstInputTfIdf, secondInputTfIdf)
+    queryVectorMagnitude = math.sqrt(calcVectorMagnitude(firstInputTfIdf))
+    secondInputVectorMagnitude = math.sqrt(
+        calcVectorMagnitude(secondInputTfIdf))
     matchPercentage = float(
         dotProduct / (queryVectorMagnitude * secondInputVectorMagnitude)) * 100
 
